bee7e7e28c32fd55076c1c3e9e98abdb24f159ff
rollrat <rollrat.cse@gmail.com>
2022-09-16 15:57:46 +0900

    ?

--- front/next.config.js
+++ front/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  poweredByHeader: false,
+  experimental: {
+    outputStandalone: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
