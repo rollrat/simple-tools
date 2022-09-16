17ea87554a1589a2643c43abea16b9c7877ebeb5
rollrat <rollrat.cse@gmail.com>
2022-09-16 14:33:46 +0900

    add .

---
+++ front/pages/_document.tsx
@@ -0,0 +1,33 @@
+import Document, { Head, Main, NextScript } from "next/document";
+import React from "react";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <html>
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="stylesheet"
+            href="https://unpkg.com/react-bootstrap-typeahead/css/Typeahead.css"
+          />
+        </Head>
+        <body>
+          <div id="root">
+            <Main />
+            <NextScript />
+          </div>
+        </body>
+      </html>
+    );
+  }
+}
